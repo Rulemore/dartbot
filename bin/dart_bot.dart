f1e700e9a327b23c5f5cfcd3abb5b2841a641ba6
Evgenii Safronov <emsafronov2004@gmail.com>
2022-12-03 16:03:52 +0300

    last teledart

--- bin/dart_bot.dart
+++ bin/dart_bot.dart
@@ -1,171 +1,40 @@
 import 'package:teledart/teledart.dart';
 import 'package:teledart/telegram.dart';
 import 'package:teledart/model.dart';
-import 'dart:io' as io;
-// var file = File();
+import 'models.dart';
+import 'data.dart';
 
 const String telegramApiToken = '5982537010:AAFr0udYAAD9G1Sjv_VMe2mK7-UHAdTHKoo';
 
 void main() async {
   final telegram = Telegram(telegramApiToken);
   final event = Event((await telegram.getMe()).username!);
-  final teledart = TeleDart(telegram, event);
+  final teledart = TeleDart(telegramApiToken, event);
   teledart.start();
-
+  var query = teledart.onInlineQuery();
   chooseSubject(teledart);
-  chooseThemeRussian(teledart);
-  chooseThemeMath(teledart);
-  chooseThemeInformatics(teledart);
-  chooseThemeSocial(teledart);
-  botPopMenu(teledart);
+  inliner(query);
 }
 
 void chooseSubject(TeleDart teledart) {
-  teledart.onCommand('start').listen((message) {
-    final List<List<KeyboardButton>> subjetcts = [
+  teledart.onCommand('start').listen((event) {
+    final List<List<InlineKeyboardButton>> subjects = [
       [
-        KeyboardButton(text: 'Русский Язык'),
-        KeyboardButton(text: 'Математика'),
+        InlineKeyboardButton(text: 'Русский Язык', callback_data: 'rus'),
+        InlineKeyboardButton(text: 'Математика', callback_data: 'math'),
       ],
       [
-        KeyboardButton(text: 'Обществознание'),
-        KeyboardButton(text: 'Информатика'),
+        InlineKeyboardButton(text: 'Обществознание', callback_data: 'social'),
+        InlineKeyboardButton(text: 'Информатика', callback_data: 'inf'),
       ]
     ];
 
-    final markup = ReplyKeyboardMarkup(
-      keyboard: subjetcts,
-    );
+    final markup = InlineKeyboardMarkup(inline_keyboard: subjects);
 
-    teledart.telegram.sendMessage(
-      message.chat.id,
-      'Выберите предмет',
-      reply_markup: markup,
-    );
+    teledart.sendMessage(event.chat.id, 'Выберите предмет', reply_markup: markup);
   });
 }
 
-void botPopMenu(TeleDart teledart) {
-  teledart.onMessage(keyword: 'Назад').listen((message) {
-    final List<List<KeyboardButton>> subjetcts = [
-      [
-        KeyboardButton(text: 'Русский Язык'),
-        KeyboardButton(text: 'Математика'),
-      ],
-      [
-        KeyboardButton(text: 'Обществознание'),
-        KeyboardButton(text: 'Информатика'),
-      ]
-    ];
-
-    final markup = ReplyKeyboardMarkup(
-      keyboard: subjetcts,
-    );
-
-    teledart.telegram.sendMessage(
-      message.chat.id,
-      'Выберите предмет',
-      reply_markup: markup,
-    );
-  });
-}
-
-void chooseThemeRussian(TeleDart teledart) {
-  teledart.onMessage(keyword: 'Русский Язык').listen((message) {
-    final List<List<KeyboardButton>> russianThemesButtons = [
-      [
-        KeyboardButton(text: 'Правописание корней'),
-        KeyboardButton(text: 'Тема 2'),
-      ],
-      [
-        KeyboardButton(text: 'Тема 3'),
-        KeyboardButton(text: 'Тема 4'),
-      ],
-      [KeyboardButton(text: 'Назад')]
-    ];
-
-    final markup = ReplyKeyboardMarkup(keyboard: russianThemesButtons);
-
-    teledart.telegram.sendMessage(
-      message.chat.id,
-      'Выберите тему',
-      reply_markup: markup,
-    );
-  });
-
-  teledart.onMessage(keyword: 'Правописание корней').listen((event) {
-    teledart.replyDocument(event, io.File('/home/rulemore/dart_bot/lib/data/russian/ex9.pptx'));
-  });
-}
-
-void chooseThemeMath(TeleDart teledart) {
-  teledart.onMessage(keyword: 'Математика').listen((message) {
-    final List<List<KeyboardButton>> russianThemesButtons = [
-      [
-        KeyboardButton(text: 'Тема 1'),
-        KeyboardButton(text: 'Тема 2'),
-      ],
-      [
-        KeyboardButton(text: 'Тема 3'),
-        KeyboardButton(text: 'Тема 4'),
-      ],
-      [KeyboardButton(text: 'Назад')]
-    ];
-
-    final markup = ReplyKeyboardMarkup(keyboard: russianThemesButtons);
-
-    teledart.telegram.sendMessage(
-      message.chat.id,
-      'Выберите тему',
-      reply_markup: markup,
-    );
-  });
-}
-
-void chooseThemeSocial(TeleDart teledart) {
-  teledart.onMessage(keyword: 'Обществознание').listen((message) {
-    final List<List<KeyboardButton>> russianThemesButtons = [
-      [
-        KeyboardButton(text: 'Тема 1'),
-        KeyboardButton(text: 'Тема 2'),
-      ],
-      [
-        KeyboardButton(text: 'Тема 3'),
-        KeyboardButton(text: 'Тема 4'),
-      ],
-      [KeyboardButton(text: 'Назад')]
-    ];
-
-    final markup = ReplyKeyboardMarkup(keyboard: russianThemesButtons);
-
-    teledart.telegram.sendMessage(
-      message.chat.id,
-      'Выберите тему',
-      reply_markup: markup,
-    );
-  });
-}
-
-void chooseThemeInformatics(TeleDart teledart) {
-  teledart.onMessage(keyword: 'Информатика').listen((message) {
-    final List<List<KeyboardButton>> russianThemesButtons = [
-      [
-        KeyboardButton(text: 'Тема 1'),
-        KeyboardButton(text: 'Тема 2'),
-      ],
-      [
-        KeyboardButton(text: 'Тема 3'),
-        KeyboardButton(text: 'Тема 4'),
-      ],
-      [KeyboardButton(text: 'Назад')]
-    ];
-
-    final markup = ReplyKeyboardMarkup(keyboard: russianThemesButtons);
-
-    teledart.telegram.sendMessage(
-      message.chat.id,
-      'Выберите тему',
-      reply_markup: markup,
-    );
-  });
+Future<void> inliner(dynamic query) async {
+  query.
 }
