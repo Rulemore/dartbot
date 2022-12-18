e6e0e543db170f3352c4c0bd2fe11c340ef1913e
Evgenii Safronov <emsafronov2004@gmail.com>
2022-12-19 01:05:33 +0300

    Stable version with content

--- bin/dart_bot.dart
+++ bin/dart_bot.dart
@@ -3,90 +3,99 @@ import 'package:teledart/telegram.dart';
 import 'package:teledart/model.dart';
 import 'package:dart_bot/data.dart';
 
-const String telegramApiToken = '5982537010:AAFr0udYAAD9G1Sjv_VMe2mK7-UHAdTHKoo';
+const String telegramApiToken = '';
 
 void main() async {
   final telegram = Telegram(telegramApiToken);
   final event = Event((await telegram.getMe()).username!);
   final teledart = TeleDart(telegramApiToken, event);
-
   teledart.start();
-  chooseSubject(teledart);
+  teledart.onCommand('start').listen((message) {
+    helloMessage(teledart, message);
+    pickSubject(teledart, message);
+  });
+  callbackHandler(teledart);
 }
 
-void chooseSubject(TeleDart teledart) async {
-  teledart.onCommand('start').listen((message) {
-    startMessage(teledart, message);
-
-    teledart.onCallbackQuery().listen((event) async {
-      if (event.data != 'back') {
-        if (subjectsList.contains(event.data)) {
-          await teledart.editMessageText(
-            'Выбранный предмет: ${subjects[event.data].toString().toLowerCase()}',
-            message_id: event.message?.message_id,
-            chat_id: event.message?.chat.id,
-            reply_markup: getMarkup(event.data!),
-          );
-        } else {
+void callbackHandler(TeleDart teledart) async {
+  teledart.onCallbackQuery().listen((event) async {
+    if (event.data != 'back') {
+      if (subjectsList.contains(event.data)) {
+        try {
           await teledart.deleteMessage(event.message?.chat.id, event.message!.message_id);
-          await teledart.sendDocument(event.message?.chat.id, files[event.data]);
-          startMessage(teledart, message);
-        }
+        } catch (e) {}
+        await teledart.sendMessage(
+            event.message?.chat.id, 'Выбранный предмет: ${subjects[event.data].toString().toLowerCase()}',
+            reply_markup: getMarkup(event.data!));
       } else {
-        await teledart.deleteMessage(event.message?.chat.id, event.message!.message_id);
-        startMessage(teledart, event.teledartMessage!);
+        try {
+          await teledart.deleteMessage(event.message?.chat.id, event.message!.message_id);
+        } catch (e) {}
+        await teledart.sendDocument(event.message?.chat.id, files[event.data]);
+        pickSubject(teledart, event.teledartMessage!);
       }
-    });
+    } else if (event.data == 'back') {
+      try {
+        await teledart.deleteMessage(event.message?.chat.id, event.message!.message_id);
+      } catch (e) {}
+      pickSubject(teledart, event.teledartMessage!);
+    }
   });
 }
 
-void startMessage(TeleDart teledart, TeleDartMessage message) {
+void helloMessage(TeleDart teledart, TeleDartMessage message) async {
+  await teledart.sendMessage(message.chat.id, 'Вас приветствует бот Mospolyschool');
+}
+
+void pickSubject(TeleDart teledart, TeleDartMessage message) async {
   final List<List<InlineKeyboardButton>> subjects = [
-    [
-      InlineKeyboardButton(text: 'Русский Язык', callback_data: 'rus'),
-      InlineKeyboardButton(text: 'Математика', callback_data: 'math')
-    ],
-    [
-      InlineKeyboardButton(text: 'Обществознание', callback_data: 'social'),
-      InlineKeyboardButton(text: 'Информатика', callback_data: 'inf')
-    ]
+    [InlineKeyboardButton(text: 'Русский Язык', callback_data: 'rus')],
+    [InlineKeyboardButton(text: 'Математика', callback_data: 'math')],
+    [InlineKeyboardButton(text: 'Обществознание', callback_data: 'social')],
+    [InlineKeyboardButton(text: 'Информатика', callback_data: 'inf')],
+    [InlineKeyboardButton(text: 'Английский язык', callback_data: 'english')]
   ];
 
-  final markup = InlineKeyboardMarkup(inline_keyboard: subjects);
-
-  teledart.sendMessage(message.chat.id, 'Выберите предмет', reply_markup: markup);
+  await teledart.sendMessage(message.chat.id, 'Выберите предмет',
+      reply_markup: InlineKeyboardMarkup(inline_keyboard: subjects));
 }
 
 InlineKeyboardMarkup getMarkup(id) {
   final List<List<InlineKeyboardButton>> rusTopics = [
-    [InlineKeyboardButton(text: 'Тема 1', callback_data: 'rus_1')],
-    [InlineKeyboardButton(text: 'Тема 2', callback_data: 'rus_2')],
-    [InlineKeyboardButton(text: 'Тема 3', callback_data: 'rus_3')],
-    [InlineKeyboardButton(text: 'Тема 4', callback_data: 'rus_4')],
+    [InlineKeyboardButton(text: 'Н и НН', callback_data: 'rus_1')],
+    [InlineKeyboardButton(text: 'Не и Ни', callback_data: 'rus_2')],
+    [InlineKeyboardButton(text: 'Правописание корней', callback_data: 'rus_3')],
     [InlineKeyboardButton(text: 'Назад', callback_data: 'back')]
   ];
 
   final List<List<InlineKeyboardButton>> mathTopics = [
-    [InlineKeyboardButton(text: 'Тема 1', callback_data: 'math_1')],
-    [InlineKeyboardButton(text: 'Тема 2', callback_data: 'math_2')],
-    [InlineKeyboardButton(text: 'Тема 3', callback_data: 'math_3')],
-    [InlineKeyboardButton(text: 'Тема 4', callback_data: 'math_4')],
+    [InlineKeyboardButton(text: 'Логарифмические уравнения', callback_data: 'math_1')],
+    [InlineKeyboardButton(text: 'Тригонометрических уравнения', callback_data: 'math_2')],
+    [InlineKeyboardButton(text: 'Экономические задачи', callback_data: 'math_3')],
+    [InlineKeyboardButton(text: 'Тригонометрия', callback_data: 'math_4')],
     [InlineKeyboardButton(text: 'Назад', callback_data: 'back')]
   ];
 
   final List<List<InlineKeyboardButton>> infTopics = [
-    [InlineKeyboardButton(text: 'Тема 1', callback_data: 'inf_1')],
-    [InlineKeyboardButton(text: 'Тема 2', callback_data: 'inf_2')],
-    [InlineKeyboardButton(text: 'Тема 3', callback_data: 'inf_3')],
-    [InlineKeyboardButton(text: 'Тема 4', callback_data: 'inf_4')],
+    [InlineKeyboardButton(text: 'Графы', callback_data: 'inf_1')],
+    [InlineKeyboardButton(text: 'Информационные модели', callback_data: 'inf_2')],
+    [InlineKeyboardButton(text: 'Информационный объем сообщения', callback_data: 'inf_3')],
+    [InlineKeyboardButton(text: 'Кодирование информации', callback_data: 'inf_4')],
     [InlineKeyboardButton(text: 'Назад', callback_data: 'back')]
   ];
 
   final List<List<InlineKeyboardButton>> socialTopics = [
-    [InlineKeyboardButton(text: 'Тема 1', callback_data: 'social_1')],
-    [InlineKeyboardButton(text: 'Тема 2', callback_data: 'social_2')],
-    [InlineKeyboardButton(text: 'Тема 3', callback_data: 'social_3')],
-    [InlineKeyboardButton(text: 'Тема 4', callback_data: 'social_4')],
+    [InlineKeyboardButton(text: 'Человек и общество 1', callback_data: 'social_1')],
+    [InlineKeyboardButton(text: 'Человек и общество 2', callback_data: 'social_2')],
+    [InlineKeyboardButton(text: 'Человек и общество 3', callback_data: 'social_3')],
+    [InlineKeyboardButton(text: 'Назад', callback_data: 'back')]
+  ];
+
+  final List<List<InlineKeyboardButton>> englishTopics = [
+    [InlineKeyboardButton(text: 'Времена', callback_data: 'english_1')],
+    [InlineKeyboardButton(text: 'Грамматика и лексика', callback_data: 'english_2')],
+    [InlineKeyboardButton(text: 'Словообразование', callback_data: 'english_3')],
+    [InlineKeyboardButton(text: 'Фразовые глаголы', callback_data: 'english_4')],
     [InlineKeyboardButton(text: 'Назад', callback_data: 'back')]
   ];
 
@@ -103,6 +112,8 @@ InlineKeyboardMarkup getMarkup(id) {
     case 'social':
       return InlineKeyboardMarkup(inline_keyboard: socialTopics);
 
+    case 'english':
+      return InlineKeyboardMarkup(inline_keyboard: englishTopics);
     default:
       return InlineKeyboardMarkup(inline_keyboard: [
         [InlineKeyboardButton(text: 'Назад', callback_data: 'back')]
--- lib/data.dart
+++ lib/data.dart
@@ -7,8 +7,30 @@ Map<String, String> subjects = {
   'inf': 'Информатика'
 };
 
-List<String> subjectsList = ['rus', 'inf', 'social', 'math'];
+List<String> subjectsList = ['rus', 'inf', 'social', 'math', 'english'];
 
 Map<String, File> files = {
-  'rus_1': File('/home/rulemore/dart_bot/lib/data/russian/ex9.pptx'),
+  'rus_1': File('/home/rulemore/dart_bot/lib/data/russian/Н и НН.pptx'),
+  'rus_2': File('/home/rulemore/dart_bot/lib/data/russian/Не и Ни.pptx'),
+  'rus_3': File('/home/rulemore/dart_bot/lib/data/russian/Правописание корней.pptx'),
+  //
+  'math_1': File('/home/rulemore/dart_bot/lib/data/math/Методы решения логарифмических уравнений.pptx'),
+  'math_2': File('/home/rulemore/dart_bot/lib/data/math/Методы решения тригонометрических уравнений.pptx'),
+  'math_3': File('/home/rulemore/dart_bot/lib/data/math/Решение экономических задач.pptx'),
+  'math_4': File('/home/rulemore/dart_bot/lib/data/math/Тригонометрия.pptx'),
+  //
+  'inf_1': File('/home/rulemore/dart_bot/lib/data/informatics/Графы.pptx'),
+  'inf_2': File('/home/rulemore/dart_bot/lib/data/informatics/Информационные модели.pptx'),
+  'inf_3': File('/home/rulemore/dart_bot/lib/data/informatics/Информационный объем сообщения.pptx'),
+  'inf_4': File('/home/rulemore/dart_bot/lib/data/informatics/Кодирование и декодирование информации.pptx'),
+  'inf_5': File('/home/rulemore/dart_bot/lib/data/informatics/Таблицы истинности.pptx'),
+  //
+  'social_1': File('/home/rulemore/dart_bot/lib/data/social/Человек и общество 1.pptx'),
+  'social_2': File('/home/rulemore/dart_bot/lib/data/social/Человек и общество 2.pptx'),
+  'social_3': File('/home/rulemore/dart_bot/lib/data/social/Человек и общество 3.pptx'),
+  //
+  'english_1': File('/home/rulemore/dart_bot/lib/data/english/Времена.pptx'),
+  'english_2': File('/home/rulemore/dart_bot/lib/data/english/Грамматика и лексика.pptx'),
+  'english_3': File('/home/rulemore/dart_bot/lib/data/english/Словообразование.pptx'),
+  'english_4': File('/home/rulemore/dart_bot/lib/data/english/Фразовые глаголы.pptx'),
 };
